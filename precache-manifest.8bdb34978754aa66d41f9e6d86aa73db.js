947598e6192df829c9bed9ef94494940e784e589
monicalaura <monicalauraburns@yahoo.com>
2020-01-25 17:37:28 +0200

    Updates

---
+++ precache-manifest.8bdb34978754aa66d41f9e6d86aa73db.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86abd4d5d67cea7109b5d9116237016d",
+    "url": "/monicalaura/quotes4u.git/index.html"
+  },
+  {
+    "revision": "fa7b0c88de7ca754af2d",
+    "url": "/monicalaura/quotes4u.git/static/css/main.81bc7fae.chunk.css"
+  },
+  {
+    "revision": "800d26c271125de05fb5",
+    "url": "/monicalaura/quotes4u.git/static/js/2.302e78e0.chunk.js"
+  },
+  {
+    "revision": "c6d01cb19213691bc81a274434d00fe7",
+    "url": "/monicalaura/quotes4u.git/static/js/2.302e78e0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "fa7b0c88de7ca754af2d",
+    "url": "/monicalaura/quotes4u.git/static/js/main.1108c6b9.chunk.js"
+  },
+  {
+    "revision": "179d86da5ffe401ca8fc",
+    "url": "/monicalaura/quotes4u.git/static/js/runtime-main.3716e1de.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/monicalaura/quotes4u.git/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "1867c42e3014a8ee0a3bac426eed5d6e",
+    "url": "/monicalaura/quotes4u.git/static/media/philoBg.1867c42e.png"
+  }
+]);
